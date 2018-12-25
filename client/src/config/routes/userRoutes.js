91d147c62a6a259c53b5d5b86d2421ff9af55602
sbramd <sabbir.ahmed@ekkdam.com>
2018-12-25 18:03:33 +0600

    Add default props and display name

--- client/src/config/routes/userRoutes.js
+++ client/src/config/routes/userRoutes.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { UserDetailScreen, UserEditingScreen, NotFoundScreen } from "screens";
 
-export default ({ match, location, history }) => (
+const UserRoutes = ({ match, location, history }) => (
 	<Switch>
 		<Route exact path={`${match.url}/:userId`} render={props => <UserDetailScreen {...props} />} />
 		<Route exact path={`${match.url}/:userId/edit`} component={UserEditingScreen} />
 		<Route component={NotFoundScreen} />
 	</Switch>
 );
+UserRoutes.displayName = "UserRoutes";
+
+export default UserRoutes;
